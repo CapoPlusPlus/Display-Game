d37427eac94ea21aa1e31ea4e671a488eb684863
bypie5 <bypie966@gmail.com>
2015-10-05 19:48:10 -0700

    Mod 1 - Enemies Take Damage
    Enemies have random letters and take damage when clicked.

---
+++ DisplayGame/Assets/Classes/Computer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Computer : MonoBehaviour {
+
+	private int Health = 10;
+	
+	void Update () {
+		if (Health < 10)
+			Destroy (gameObject);
+	}
+
+	void OnCollision2D (Collision2D collision) {
+		print ("Touched");
+		StartCoroutine (TakeDamage ());
+	}
+
+	IEnumerator TakeDamage () {
+		float timer = 0f;
+		while (timer < 3f) {
+			Health--;
+			timer += Time.deltaTime;
+			yield return null;
+		}
+	}
+}
---
+++ DisplayGame/Assets/Classes/EnemyLetter.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLetter : MonoBehaviour {
+
+	public GameObject[] letterMeshes = new GameObject[6];
+
+	private GameObject thisMesh;
+	private Vector3 target;
+	private int Health = 3; 
+
+	void Awake () {
+		thisMesh = Instantiate (letterMeshes[Random.Range (0, letterMeshes.Length)] , transform.position, Quaternion.identity) as GameObject;
+		thisMesh.transform.parent = transform;
+		target = GameObject.Find("Computer").transform.position;
+	}
+
+	void Update () {
+		moveTowardsTarget ();
+	}
+
+	void OnMouseDown() {
+		StartCoroutine (TakeDamage ());
+	}
+
+	IEnumerator TakeDamage() {
+		Health--;
+		if (Health <= 0)
+			Destroy (gameObject);
+		//Shake animation
+		float timer = 0f;
+		while (timer < .25f) {
+			Vector3 shake = new Vector3 (Mathf.Sin (Time.time * 1000f) * .1f, 0, 0);
+			transform.position += shake;
+			timer += Time.deltaTime;
+			yield return null;
+		}
+	}
+	
+	void moveTowardsTarget () {
+		transform.position = Vector3.MoveTowards (transform.position, target, 1.25f * Time.deltaTime);
+	}
+}
